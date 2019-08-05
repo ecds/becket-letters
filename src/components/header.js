4f232a41f4c87f3adb38d28e93c2b7d45b1eb8b1
MTHERRI <mtherri@emory.edu>
2019-08-05 11:07:19 -0400

    corrected bootstrap-react to react-bootstrap in header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col } from 'bootstrap-react';
+import { Row, Col } from 'react-bootstrap';
 
 export class Header extends Component {
     render() {
