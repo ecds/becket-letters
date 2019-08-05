6553e5329ce6040743c0178bc70e8b4d5f6e4296
MTHERRI <mtherri@emory.edu>
2019-08-05 12:05:06 -0400

    added location-path file skeleton as discrete component

---
+++ src/components/location-path.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Row } from 'react-bootstrap';
+import '../styles/location-path.css'
+
+export class LocationPath extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+                        )
+                    }
+                }
+                
+export default LocationPath;
