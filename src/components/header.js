ae0140155677713a6279e9ca42a05e45a20f7697
MTHERRI <mtherri@emory.edu>
2019-08-05 11:10:52 -0400

    created temporary header.css

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
 import { Row, Col } from 'react-bootstrap';
+import '../styles/header.css';
 
 export class Header extends Component {
     render() {
         return (
             <Row>
-                <Col>
-                    Hi
-            </Col>
+                
             </Row>
         )
     }
