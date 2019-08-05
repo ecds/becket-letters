e41f8bef0f5c9617e2139009eaa132bcdaaddfd1
MTHERRI <mtherri@emory.edu>
2019-08-05 11:04:42 -0400

    created header.js skeleton

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Row, Col } from 'bootstrap-react';
+
+export class Header extends Component {
+    render() {
+        return (
+            <Row>
+                <Col>
+                    Hi
+            </Col>
+            </Row>
+        )
+    }
+}
+
+export default Header;
