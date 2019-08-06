121df46eba5b765d904b23d42f49a1accd57374b
MTHERRI <mtherri@emory.edu>
2019-08-06 10:11:31 -0400

    created LetterPg component structure

---
+++ src/components/letter.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Row, Col } from 'react-bootstrap';
+
+export class LetterPg extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Letter Details</h1>
+      </div>
+    )
+  }
+}
+
+export default LetterPg;
