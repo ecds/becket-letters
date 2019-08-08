7af512b69eebaeb56e5d8db356b4355d9a29ce10
MTHERRI <mtherri@emory.edu>
2019-08-08 10:36:56 -0400

    fixed 2008.json, letter.js now works with api call to 2008.json

--- src/components/letter.js
+++ src/components/letter.js
@@ -20,7 +20,7 @@ export class LetterPg extends Component {
         const letter = getLetterData.data;
         this.setState({ letter });
         console.log(letter)
-        console.log(this.state.letter[0].Code)
+        console.log(this.state.letter[0]['PrimaryLang'])
         this.setState({ isLoaded: true })
       }))
       .catch((err) => {
@@ -48,7 +48,7 @@ export class LetterPg extends Component {
                   <Col md={6} className='letterPgDetails'>
                       <Row>
                           <Col>
-                              <Accordion defaultActiveKey="1">
+                              <Accordion defaultActiveKey="3">
                                   <Card>
                                       <Accordion.Toggle className='detInfo' as={Card.Header} eventKey="0">
                                           <h4 className='accordTitle'>Details</h4>
@@ -56,7 +56,7 @@ export class LetterPg extends Component {
                                       <Accordion.Collapse eventKey="0">
                                           <Card.Body className='results'>
                                               <p>ID: </p><p> {this.state.letter[0].ID}</p>
-                                              <p>Code: </p><p> "SABE [SPRING 1957] ALBO"</p>
+                                              <p>Code: </p><p>{this.state.letter[0].Code}</p>
                                           </Card.Body>
                                       </Accordion.Collapse>
                                   </Card>
@@ -67,29 +67,29 @@ export class LetterPg extends Component {
                                       </Accordion.Toggle>
                                       <Accordion.Collapse eventKey="1">
                                           <Card.Body className='results'>
-                                              <p>Day: </p><p> 0</p>
-                                              <p>Month: </p><p> 0</p>
-                                              <p>Year: </p><p> 57</p>
-                                              <p>Addressed from (Actual): </p><p> </p>
-                                              <p>Reg. Place written: </p><p> </p>
-                                              <p>Reg. Place written city: </p><p> </p>
-                                              <p>Reg. Place written country: </p><p> </p>
-                                              <p>Reg. Place written, second city: </p><p> </p>
-                                              <p>Autograph or Typed: </p><p> </p>
-                                              <p>PhysDes: </p><p> "Other"</p>
-                                              <p>initialed or signed: </p><p> </p>
-                                              <p>Postcard Image: </p><p> </p>
-                                              <p>phys descr detail: </p><p> </p>
-                                              <p>PhysDes notes: </p><p> </p>
+                                              <p>Day: </p><p>{this.state.letter[0].Day}</p>
+                                              <p>Month: </p><p>{this.state.letter[0].Month}</p>
+                                              <p>Year: </p><p>{this.state.letter[0].Year}</p>
+                                              <p>Addressed from (Actual): </p><p>{this.state.letter[0]['Addressed from (Actual)']}</p>
+                                              <p>Reg. Place written: </p><p>{this.state.letter[0]['Reg. Place written']}</p>
+                                              <p>Reg. Place written city: </p><p>{this.state.letter[0]['Reg. Place written city']}</p>
+                                              <p>Reg. Place written country: </p><p>{this.state.letter[0]['Reg. Place written country']}</p>
+                                              <p>Reg. Place written, second city: </p><p>{this.state.letter[0]['Reg. Place written, second city']}</p>
+                                              <p>Autograph or Typed: </p><p>{this.state.letter[0]['Autograph or Typed']}</p>
+                                              <p>PhysDes: </p><p>{this.state.letter[0].PhysDes}</p>
+                                              <p>initialed or signed: </p><p>{this.state.letter[0]['initialed or signed']}</p>
+                                              <p>Postcard Image: </p><p>{this.state.letter[0]['Postcard Image']}</p>
+                                              <p>phys descr detail: </p><p>{this.state.letter[0]['phys descr detail']}</p>
+                                              <p>PhysDes notes: </p><p>{this.state.letter[0]['PhysDes notes']}</p>
                                               <p>leaves: </p><p> "?"</p>
-                                              <p>sides: </p><p> "?"</p>
-                                              <p>Envelope: </p><p> </p>
+                                              <p>sides: </p><p>{this.state.letter[0].sides}</p>
+                                              <p>Envelope: </p><p>{this.state.letter[0].Envelope}</p>
                                               <p>Postmark (Actual): </p><p> </p>
-                                              <p>Addressed to (Actual): </p><p> "ALAIN BOSQUET"</p>
-                                              <p>Reg. recipient: </p><p> "Alain Bosquet"</p>
-                                              <p>Reg place sent: </p><p> </p>
-                                              <p>Reg. PlaceSent City: </p><p> </p>
-                                              <p>Reg. PlaceSent Country: </p><p> </p>
+                                              <p>Addressed to (Actual): </p><p>{this.state.letter[0]['Addressed to (Actual)']}</p>
+                                              <p>Reg. recipient: </p><p>{this.state.letter[0]['Reg. recipient']}</p>
+                                              <p>Reg place sent: </p><p>{this.state.letter[0]['Reg place sent']}</p>
+                                              <p>Reg. PlaceSent City: </p><p>{this.state.letter[0]['Reg PlaceSent City']}</p>
+                                              <p>Reg. PlaceSent Country: </p><p>{this.state.letter[0]['Reg. PlaceSent Country']}</p>
                                           </Card.Body>
                                       </Accordion.Collapse>
                                   </Card>
@@ -100,7 +100,7 @@ export class LetterPg extends Component {
                                       </Accordion.Toggle>
                                       <Accordion.Collapse eventKey="2">
                                           <Card.Body>
-                                              <p> "BY BOSQUET - 1996 - REPRINTED FROM LES FRUITS. . ."</p>
+                                              <p>{this.state.letter[0].Additional}</p>
                                           </Card.Body>
                                       </Accordion.Collapse>
                                   </Card>
@@ -111,26 +111,26 @@ export class LetterPg extends Component {
                                       </Accordion.Toggle>
                                       <Accordion.Collapse eventKey="3">
                                           <Card.Body className='results'>
-                                              <p>First Repository: </p><p> "BOSQUET"</p>
-                                              <p>First Format: </p><p> </p>
-                                              <p>Euro or Am?: </p><p> </p>
-                                              <p>First Public?: </p><p> </p>
-                                              <p>First Collection: </p><p> </p>
-                                              <p>Repository information: </p><p> </p>
-                                              <p>Second Repository: </p><p> </p>
-                                              <p>Second Format: </p><p> </p>
-                                              <p>Second Public?: </p><p> </p>
-                                              <p>Second Collection: </p><p> </p>
-                                              <p>Third Repository: </p><p> </p>
-                                              <p>Third Format: </p><p> </p>
-                                              <p>Third Public?: </p><p> </p>
-                                              <p>Third Collection: </p><p> </p>
-                                              <p>OwnerRights: </p><p> "Beckett"</p>
-                                              <p>PrimaryLang: </p><p> "French"</p>
-                                              <p>File: </p><p> "BOSQUET"</p>
-                                              <p>Sender: </p><p> "Samuel Beckett"</p>
-                                              <p>PlacePrevPubl: </p><p> "LES FRUITS DE L'AN DERNIE"</p>
-                                              <p>Verified: </p><p> "Y"</p>
+                                              <p>First Repository: </p><p>{this.state.letter[0]['First Repository']}</p>
+                                              <p>First Format: </p><p>{this.state.letter[0]['First Format']}</p>
+                                              <p>Euro or Am?: </p><p>{this.state.letter[0]['Euro or Am?']}</p>
+                                              <p>First Public?: </p><p>{this.state.letter[0]['First Public?']}</p>
+                                              <p>First Collection: </p><p>{this.state.letter[0]['First Collection']}</p>
+                                              <p>Repository information: </p><p>{this.state.letter[0]['Repository information']}</p>
+                                              <p>Second Repository: </p><p>{this.state.letter[0]['Second Repository']}</p>
+                                              <p>Second Format: </p><p>{this.state.letter[0]['Second Format']}</p>
+                                              <p>Second Public?: </p><p>{this.state.letter[0]['Second Public?']}</p>
+                                              <p>Second Collection: </p><p>{this.state.letter[0]['Second Collection']}</p>
+                                              <p>Third Repository: </p><p>{this.state.letter[0]['Third Repository']}</p>
+                                              <p>Third Format: </p><p>{this.state.letter[0]['Third Format']}</p>
+                                              <p>Third Public?: </p><p>{this.state.letter[0]['Third Public?']}</p>
+                                              <p>Third Collection: </p><p>{this.state.letter[0]['Third Collection']}</p>
+                                              <p>OwnerRights: </p><p>{this.state.letter[0].OwnerRights}</p>
+                                              <p>PrimaryLang: </p><p>{this.state.letter[0].PrimaryLang}</p>
+                                              <p>File: </p><p>{this.state.letter[0].File}</p>
+                                              <p>Sender: </p><p>{this.state.letter[0].Sender}</p>
+                                              <p>PlacePrevPubl: </p><p>{this.state.letter[0].PlacePrevPubl}</p>
+                                              <p>Verified: </p><p>{this.state.letter[0]['Verified']}</p>
                                           </Card.Body>
                                       </Accordion.Collapse>
                                   </Card>
