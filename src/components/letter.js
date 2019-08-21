bac1a36a7c35d87c18f46e59b6867ac2425523e4
MTHERRI <mtherri@emory.edu>
2019-08-21 14:34:13 -0400

    added more fields to letter.js

--- src/components/letter.js
+++ src/components/letter.js
@@ -38,49 +38,58 @@ export class LetterPg extends Component {
             // return now that component has value
         } else {
             // 
-            // to:
-            // {recipient} at {address}
+            // recipient person/place
+            let receivedBy;
+            if (this.state.letter[500]["Reg. recipient"] === '') {
+                receivedBy = null
+            }
+            else receivedBy = 'by ' + this.state.letter[500]["Reg. recipient"];
+            let receivedAt;
+            if (this.state.letter[500]['Reg place sent'] === '') {
+                receivedAt = null
+            }
+            else receivedAt = 'at ' + this.state.letter[500]['Reg place sent'];
             // 
             // autographed or typed
             let formatAndSignoff;
-            if (this.state.letter[10]['Autograph or Typed'] === '' && this.state.letter[10]['initialed or signed'] === '') {
+            if (this.state.letter[500]['Autograph or Typed'] === '' && this.state.letter[500]['initialed or signed'] === '') {
                 formatAndSignoff = null
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'A' && this.state.letter[10]['initialed or signed'] === '') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'A' && this.state.letter[500]['initialed or signed'] === '') {
                 formatAndSignoff = 'Beckett hand wrote this letter.'
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'A' && this.state.letter[10]['initialed or signed'] === 'I') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'A' && this.state.letter[500]['initialed or signed'] === 'I') {
                 formatAndSignoff = "Beckett hand wrote this letter and initialed his name."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'A' && this.state.letter[10]['initialed or signed'] === 'S') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'A' && this.state.letter[500]['initialed or signed'] === 'S') {
                 formatAndSignoff = "Beckett hand wrote this letter and signed his name."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'T' && this.state.letter[10]['initialed or signed'] === '') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'T' && this.state.letter[500]['initialed or signed'] === '') {
                 formatAndSignoff = "Beckett typed this letter."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'T' && this.state.letter[10]['initialed or signed'] === 'I') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'T' && this.state.letter[500]['initialed or signed'] === 'I') {
                 formatAndSignoff = "Beckett typed this letter and signed his name."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === 'T' && this.state.letter[10]['initialed or signed'] === 'S') {
+            else if (this.state.letter[500]['Autograph or Typed'] === 'T' && this.state.letter[500]['initialed or signed'] === 'S') {
                 formatAndSignoff = "Beckett typed this letter and signed his name."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === '' && this.state.letter[10]['initialed or signed'] === 'I') {
+            else if (this.state.letter[500]['Autograph or Typed'] === '' && this.state.letter[500]['initialed or signed'] === 'I') {
                 formatAndSignoff = "Beckett initialed his name."
             }
-            else if (this.state.letter[10]['Autograph or Typed'] === '' && this.state.letter[10]['initialed or signed'] === 'S') {
+            else if (this.state.letter[500]['Autograph or Typed'] === '' && this.state.letter[500]['initialed or signed'] === 'S') {
                 formatAndSignoff = "Beckett signed his name."
             }
             //
             let leavesAndSides;
-            let leavesNo = this.state.letter[10]['leaves'];
-            let sidesNo = this.state.letter[10]['sides'];
-            if (this.state.letter[10]['leaves'] === '' && this.state.letter[10]['sides'] === '') {
+            let leavesNo = this.state.letter[500]['leaves'];
+            let sidesNo = this.state.letter[500]['sides'];
+            if (this.state.letter[500]['leaves'] === '' && this.state.letter[500]['sides'] === '') {
                 leavesAndSides = null
             }
-            else if (this.state.letter[10]['leaves'] === '' && this.state.letter[10]['sides'] === !'') {
+            else if (this.state.letter[500]['leaves'] === '' && this.state.letter[500]['sides'] === !'') {
                 leavesAndSides = 'This letter has {sides} sides.'
             }
-            else if (this.state.letter[10]['leaves'] === !'' && this.state.letter[10]['sides'] === '') {
+            else if (this.state.letter[500]['leaves'] === !'' && this.state.letter[500]['sides'] === '') {
                 leavesAndSides = 'This letter has {leaves} leaves.'
             }
             else leavesAndSides = 'This letter has ' + leavesNo + ' leave(s) and ' + sidesNo + ' side(s).'
@@ -92,34 +101,44 @@ export class LetterPg extends Component {
             // 
             // 
             return (
-                <div>
+                <Container>
                     <Row>
-                        <h2>Letter from Samuel Beckett to {this.state.letter[10]['Reg. recipient']} on {this.state.letter[10].Day}/{this.state.letter[10].Month}/{this.state.letter[10].Year}</h2>
+                        <h2>Letter from Samuel Beckett to {this.state.letter[500]['Reg. recipient']} on {this.state.letter[500].Day}/{this.state.letter[500].Month}/{this.state.letter[500].Year}</h2>
                     </Row>
                     <Row>
                         <ProfileLite personId="9a304912-c851-436a-ae20-e72e73f92397" />
                     </Row>
                     <Row>
-                        <Col md={12} className='letterPgDetails'>
+                        <Col md='auto' className='letterPgDetails'>
 
 
                             <div className='letterInfo'>
                                 <h4>Information about this letter:</h4>
                                 <h5>This letter was written at:</h5>
-                                <p>{this.state.letter[10]['Reg. Place written']}</p>
-                                <p>{this.state.letter[10]['Reg. Place written city']}, {this.state.letter[10]['Reg. Place written country']}</p>
+                                <p>{this.state.letter[500]['Reg. Place written']}</p>
+                                <p>{this.state.letter[500]['Reg. Place written city']}, {this.state.letter[500]['Reg. Place written country']}</p>
                                 <h5>Beckett addressed it from:</h5>
-                                <p>{this.state.letter[10]['Addressed from (Actual)']}</p>
+                                <p>{this.state.letter[500]['Addressed from (Actual)']}</p>
+                                <h5>It was received:</h5>
+                                <p>{receivedBy}</p>
+                                <p>{receivedAt}</p>
+                                <p>{this.state.letter[500]['Reg. place sent']}</p>
                                 <h5>{formatAndSignoff}</h5>
+                                <h5>Language:</h5>
+                                <p>{this.state.letter[500]['PrimaryLang']}</p>
                                 <h5>{leavesAndSides}</h5>
-
+                                
                             </div>
-                            <h5>Postcard Image</h5>
-                            <p>{this.state.letter[10]['Postcard Image']}</p>
+                            <h5>Location of original letter:</h5>
+                            <p>{this.state.letter[500]['First Repository']}</p>
+                            <p>({this.state.letter[500]['Euro or Am?']})</p>
+                            <h5>Availability:</h5>
+                            <p>{this.state.letter[500]['First Public?']}</p>
+                            
 
                         </Col>
                     </Row>
-                </div>
+                </Container>
             )
         }
     }
