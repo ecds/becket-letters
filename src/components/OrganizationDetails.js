4902fd551057047eeebe0bdcfb0896357661cc54
MTHERRI <mtherri@emory.edu>
2019-10-22 16:21:03 -0400

    fixed organization letters table

--- src/components/OrganizationDetails.js
+++ src/components/OrganizationDetails.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import AlternateSpellings from './utilities/AlternateSpellings';
 import LetterQuickGlance from './LetterQuickGlance';
+import AlternateSpellings from './utilities/AlternateSpellings';
+import SearchRecipientOnPage from './utilities/SearchRecipientOnPage';
+import MentionedLetters from './utilities/MentionedLettersTable';
 
 class OrganizationDetails extends Component {
 
@@ -26,6 +28,7 @@ class OrganizationDetails extends Component {
       axios.get(this.props.apiUrl+'/entities/'+this.props.match.params.id)])
       .then(axios.spread((getData) => {
         const entityData = getData.data.data;
+        console.log(entityData)
         this.setState({ entityData });
         this.setState({ isLoaded: true })
       }))
@@ -34,6 +37,7 @@ class OrganizationDetails extends Component {
         this.setState({ error: err.message });
       });
   }
+
 
   render() {
     const { error, isLoaded } = this.state;
@@ -47,10 +51,28 @@ class OrganizationDetails extends Component {
     } else {
       return (
         <div className="details">
-          <h1 dangerouslySetInnerHTML={{__html: this.state.entityData.attributes.label}}/>
-          <div>{this.state.entityData.attributes.properties['description']}</div>
-            <AlternateSpellings spellingList={this.state.entityData.attributes.properties['alternate-spellings']}/>
-        </div>
+          <h1 dangerouslySetInnerHTML={{ __html: this.state.entityData.attributes.label }} />
+          <table className="table table-striped">
+            <tbody>
+              <tr>
+                <td>Description</td>
+                <td>{this.state.entityData.attributes.properties.description}</td>
+              </tr>
+            </tbody>
+          </table>
+          <h2>Letters <span dangerouslySetInnerHTML={{ __html: this.state.entityData.attributes.label }} /> is Mentioned In:</h2>
+          <SearchRecipientOnPage tableId='repositoryLetters' placeHolder='by recipient' />
+          <table className='table table-bordered' id='repositoryLetters'>
+            <thead>
+              <tr>
+                <th>Recipient(s)</th>
+                <th colSpan="2">Date</th>
+              </tr>
+            </thead>
+          <MentionedLetters letters={this.state.entityData.attributes['public-letters-hash']} />
+
+          </table>
+        </div >
       )
     }
   }
