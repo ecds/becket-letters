41e79de4a0a7404b2dadfabce120978bb74c2f60
MTHERRI <mtherri@emory.edu>
2019-10-24 11:58:09 -0400

    added react-document-meta and set it up on landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,17 +2,26 @@ import React, { Component } from "react";
 import HomeJumbotron from './Jumbotron';
 import SearchPage from './SearchPage';
 import HomeTiles from './utilities/HomeTiles';
+import DocumentMeta from 'react-document-meta';
 
 class Landing extends Component {
-    render() {
-        return (
-          <div className="landing">
-            <HomeJumbotron/>
-            <SearchPage/>
-            <HomeTiles/>
-          </div>
-        )
-    }
+  render() {
+    const meta = {
+      title: 'Beckett Letters Landing Page',
+      description: "Begin your exploration of Samuel Beckett's letters",
+    };
+
+    return (
+      <div className="landing" >
+        <DocumentMeta {...meta}>
+          <h1>Hello World!</h1>
+        </DocumentMeta>
+        <HomeJumbotron />
+        <SearchPage />
+        <HomeTiles />
+      </div >
+    )
+  }
 }
 
 export default Landing;
