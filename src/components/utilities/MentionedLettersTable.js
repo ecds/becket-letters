0c83df5344b0c3d5f99bc52001a78d19ce5edfe3
MTHERRI <mtherri@emory.edu>
2019-10-25 13:25:39 -0400

    added condition for no letters returned

--- src/components/utilities/MentionedLettersTable.js
+++ src/components/utilities/MentionedLettersTable.js
@@ -3,17 +3,29 @@ import { Link } from "react-router-dom";
 
 class MentionedLetters extends Component {
     render() {
-        return (
-            <tbody>
-                {this.props.letters.map((letter) =>
+        if (this.props.letters.length === 0) {
+            return (
+                <tbody>
                     <tr>
-                        <td>{letter['recipients'].map((this_recipient) => <a href={'/people/' + this_recipient.id + '/' + this_recipient.name}>{this_recipient.name}</a>)}</td>
-                        <td>{letter.date}</td>
-                        <td className="actions"><Link to={'/letters/letterdetails/' + letter.id}>Explore Letter</Link></td>
+                        <td>No letters!</td>
+                        <td></td>
                     </tr>
-                )}
-            </tbody>
-        )
+                </tbody>
+            )
+        }
+        else {
+            return (
+                <tbody>
+                    {this.props.letters.map((letter) =>
+                        <tr>
+                            <td>{letter['recipients'].map((this_recipient) => <a href={'/people/' + this_recipient.id + '/' + this_recipient.name}>{this_recipient.name}</a>)}</td>
+                            <td>{letter.date}</td>
+                            <td className="actions"><Link to={'/letters/letterdetails/' + letter.id}>Explore Letter</Link></td>
+                        </tr>
+                    )}
+                </tbody>
+            )
+        }
     }
 }
 
