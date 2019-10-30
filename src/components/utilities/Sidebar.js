76417d64fd010639c21f165bda1b52ba36f46462
MTHERRI <mtherri@emory.edu>
2019-10-30 15:14:36 -0400

    alphabetized navdropdown order

--- src/components/utilities/Sidebar.js
+++ src/components/utilities/Sidebar.js
@@ -10,21 +10,21 @@ class Sidebar extends Component {
         <Navbar.Brand href="#home">Samuel Beckett Letters</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Browse Letters By" id="basic-nav-dropdown">
-              <NavDropdown.Item href='/browse-letters-places'>Letters by Places</NavDropdown.Item>
-              <NavDropdown.Item href='/browse-letters-people'>Letters by People</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-productions">Letters by Productions</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-organizations">Letters by Organizations</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-publications">Letters by Publications</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-music">Letters by Music</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-readings">Letters by Reading</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-writings">Letters by Writings</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-translations">Letters by Translations</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-attendance">Letters by Attendance</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-works_of_art">Letters by Works of Art</NavDropdown.Item>
-              <NavDropdown.Item href="/browse-letters-public_events">Letters by Public Events</NavDropdown.Item>
-            </NavDropdown>
+          <Nav.Link href="/">Home</Nav.Link>
+          <NavDropdown title="Browse Letters By" id="basic-nav-dropdown">
+            <NavDropdown.Item href="/browse-letters-attendance">Letters by Attendance</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-music">Letters by Music</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-organizations">Letters by Organizations</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-productions">Letters by Productions</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-public_events">Letters by Public Events</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-publications">Letters by Publications</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-readings">Letters by Reading</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-translations">Letters by Translations</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-works_of_art">Letters by Works of Art</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-writings">Letters by Writings</NavDropdown.Item>
+            <NavDropdown.Item href='/browse-letters-people'>Letters by People</NavDropdown.Item>
+            <NavDropdown.Item href='/browse-letters-places'>Letters by Places</NavDropdown.Item>
+          </NavDropdown>
         </Navbar.Collapse>
       </Navbar>
 
