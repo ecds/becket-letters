a835085a7fc2e1c41db757636a3d150e1b0991f5
MTHERRI <mtherri@emory.edu>
2019-11-04 13:20:47 -0500

    alphabetized browseby tab order

--- src/components/BrowseLetters.js
+++ src/components/BrowseLetters.js
@@ -22,20 +22,18 @@ class BrowseLetters extends Component {
       <div className="pt-3">
         <h1>Browse Letters by Entities Mentioned</h1>
         <ul className="nav nav-tabs" id="browse-tabs">
-          <li className={this.setActive('by-places')}>
-            <Link to='/browse-letters-places'>Places</Link>
-          </li>
-          <li className={this.setActive('by-people')}><Link to="/browse-letters-people">People</Link></li>
-          <li className={this.setActive('by-organization')}><Link to="/browse-letters-organizations">Organizations</Link></li>
-          <li className={this.setActive('by-production')}><Link to="/browse-letters-productions">Productions</Link></li>
-          <li className={this.setActive('by-publication')}><Link to="/browse-letters-publications">Publications</Link></li>
-          <li className={this.setActive('by-music')}><Link to="/browse-letters/-music">Music</Link></li>
-          <li className={this.setActive('by-reading')}><Link to="/browse-letters/-readings">Readings</Link></li>
-          <li className={this.setActive('by-writing')}><Link to="/browse-letters-writings">Writings</Link></li>
-          <li className={this.setActive('by-translations')}><Link to="/browse-letters-translations">Translations</Link></li>
           <li className={this.setActive('by-attendance')}><Link to="/browse-letters-attendance">Attendance</Link></li>
-          <li className={this.setActive('by-works-of-art')}><Link to="/browse-letters-works_of_art">Works of Art</Link></li>
+          <li className={this.setActive('by-music')}><Link to="/browse-letters/-music">Music</Link></li>
+          <li className={this.setActive('by-organization')}><Link to="/browse-letters-organizations">Organizations</Link></li>
+          <li className={this.setActive('by-people')}><Link to="/browse-letters-people">People</Link></li>
+          <li className={this.setActive('by-places')}><Link to="/browse-letters-places">Places</Link></li>
+          <li className={this.setActive('by-production')}><Link to="/browse-letters-productions">Productions</Link></li>
           <li className={this.setActive('by-public-events')}><Link to="/browse-letters-public_events">Public Events</Link></li>
+          <li className={this.setActive('by-publication')}><Link to="/browse-letters-publications">Publications</Link></li>
+          <li className={this.setActive('by-reading')}><Link to="/browse-letters/-readings">Readings</Link></li>
+          <li className={this.setActive('by-translations')}><Link to="/browse-letters-translations">Translations</Link></li>
+          <li className={this.setActive('by-works-of-art')}><Link to="/browse-letters-works_of_art">Works of Art</Link></li>
+          <li className={this.setActive('by-writing')}><Link to="/browse-letters-writings">Writings</Link></li>
         </ul>
 
       </div>
