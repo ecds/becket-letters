028a19c935dbb859d405297b08da771a974b72bf
MTHERRI <mtherri@emory.edu>
2020-10-16 14:56:12 -0400

    fixed image

--- src/components/utilities/Sidebar.js
+++ src/components/utilities/Sidebar.js
@@ -12,7 +12,7 @@ class Sidebar extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav.Link href="/">Home</Nav.Link>
           <NavDropdown title="Browse Letters By" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/browse-letters-attendance">Attendances</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-attendance">Attendance</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-music">Music</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-organization">Organizations</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-person">People</NavDropdown.Item>
@@ -20,7 +20,7 @@ class Sidebar extends Component {
             <NavDropdown.Item href="/browse-letters-production">Productions</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-public-event">Public Events</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-publication">Publications</NavDropdown.Item>
-            <NavDropdown.Item href="/browse-letters-reading">Readings</NavDropdown.Item>
+            <NavDropdown.Item href="/browse-letters-reading">Reading</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-repositories">Repository</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-Translating">Translatings</NavDropdown.Item>
             <NavDropdown.Item href="/browse-letters-work-of-art">Works of Art</NavDropdown.Item>
