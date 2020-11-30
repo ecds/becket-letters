e529982f294d5e8b6fe4558bbaa366a109b02fa6
MTHERRI <mtherri@emory.edu>
2020-11-30 13:50:26 -0500

    image error handler added

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
-import {Col, Row} from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 class TimelineEvent extends Component {
+
+  timelineImageAdder(image_url) {
+
+    fetch(image_url, { method: 'HEAD' })
+      .then(res => {
+        if (res.ok) {
+          console.log('Image exists.');
+          return (
+            <img className='timeline-event-image' src={this.props.event.additional} alt='timeline event' />
+          )
+        } else {
+          console.log('Image does not exist.');
+          return null
+        }
+      }).catch(err => console.log('Error:', err));
+
+  }
+
   render() {
     const eventClassName = (() => {
       if (this.props.event.type === 'global')
@@ -16,7 +34,7 @@ class TimelineEvent extends Component {
           <div className={this.props.event.type}>
             <h4>{this.props.event.date}</h4>
             <p><span dangerouslySetInnerHTML={{ __html: this.props.event.event }} /></p>
-            {this.props.event.additional && this.props.event.additional.startsWith('http') ? <img className='timeline-event-image' src={this.props.event.additional} alt='timeline event' /> : null}
+            {this.props.event.additional && this.props.event.additional.startsWith('http') ? this.timelineImageAdder(this.props.event.additional) : null}
           </div>
         </Col>
       </Row>
